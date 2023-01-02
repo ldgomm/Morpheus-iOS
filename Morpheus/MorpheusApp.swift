c2cf36ae456ca919cb30297a115a8ec4ac23bb4e
José Ruiz <ruiz211@duck.com>
2023-01-02 16:10:28 -0500

    Initial commit

---
+++ Morpheus/MorpheusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorpheusApp.swift
+//  Morpheus
+//
+//  Created by José Ruiz on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MorpheusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
